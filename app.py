ce6bf2e2f6b657a5c4f54f5cb44b0d441360c3df
CodeRishiX <debangshubhattacharya4@gmail.com>
2025-01-18 19:28:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-# Download required NLTK resources (only needs to be done once)
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Initialize Porter Stemmer
+
 ps = PorterStemmer()
 
-# Text transformation function
+
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
@@ -37,27 +37,27 @@ def transform_text(text):
 
     return " ".join(y)
 
-# Load vectorizer and model
+
 vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Streamlit UI
+
 st.title('Email/Spam Classifier')
 
-# Input from user
-input_sms = st.text_input('Enter your Message')
+
+input_sms = st.text_input('Kindly Enter your Message -')
 
 if st.button('Classify'):
-    # Preprocess input
+   
     transformed_sms = transform_text(input_sms)
 
-    # Vectorize input
+  
     vector_input = vectorizer.transform([transformed_sms])
 
-    # Predict using the model
+    
     result = model.predict(vector_input)[0]
 
-    # Display result
+   
     if result == 1:
         st.header('Spam')
     else:
